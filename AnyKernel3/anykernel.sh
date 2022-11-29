32217fbe5012c7e068b05edc0826338174363f9e
COSMIC <rayavanindra@gmail.com>
2022-11-29 23:33:16 +0530

    miatoll: Update kernel string
    Signed-off-by: COSMIC <rayavanindra@gmail.com>

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=DFC Kernel by Sairam60
+kernel.string=DFC Kernel by Sairam60 (Upstreamed by COSMIC)
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
